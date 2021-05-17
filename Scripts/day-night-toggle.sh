911e7373c3945937394cdeed83c2bb4db82d37cc
rapteon <surajnaranatt@gmail.com>
2021-05-17 07:54:17 +0530

    Fixed buggy behaviour of script which destroyed one config while switching modes.

--- Scripts/day-night-toggle.sh
+++ Scripts/day-night-toggle.sh
@@ -2,23 +2,25 @@
 
 hour=$(date +%H)
 
-if [ $hour -ge 19 ]
+if [ $hour -ge 19 -a $hour -le 7 ]
 then
 	# Changing i3 config
-	cp ~/.config/i3/config ~/.config/i3/dayconfig
 	cp ~/.config/i3/nightconfig ~/.config/i3/config
 
 	# Changing gtk config
-	cp ~/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/day-settings.ini
 	cp ~/.config/gtk-3.0/night-settings.ini ~/.config/gtk-3.0/settings.ini
+
+	# Changing kitty config
+	cp ~/.config/kitty/night-kitty.conf ~/.config/kitty/kitty.conf
 else
 	# Changing i3 config
-	cp ~/.config/i3/config ~/.config/i3/nightconfig
 	cp ~/.config/i3/dayconfig ~/.config/i3/config
 
 	# Changing gtk config
-	cp ~/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/night-settings.ini
 	cp ~/.config/gtk-3.0/day-settings.ini ~/.config/gtk-3.0/settings.ini
+
+	# Changing kitty config
+	cp ~/.config/kitty/day-kitty.conf ~/.config/kitty/kitty.conf
 fi
 
 i3-msg restart > /dev/null
